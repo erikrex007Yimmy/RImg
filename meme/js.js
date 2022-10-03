c864526028a28bfb624cce6f1ba40171936c2036
erikrex007Yimmy <113575948+erikrex007Yimmy@users.noreply.github.com>
2022-10-03 20:39:06 +0200

    Update js.js

--- meme/js.js
+++ meme/js.js
@@ -1,6 +1,16 @@
 
 const h = [
-    "https://erikrex007yimmy.github.io/RImg/meme/imgs/skeleton-dancing.gif"
+    "https://erikrex007yimmy.github.io/RImg/meme/imgs/skeleton-dancing.gif",
+    "https://erikrex007yimmy.github.io/RImg/meme/imgs/cuddle.gif",
+    "https://erikrex007yimmy.github.io/RImg/meme/imgs/cute-baby-mochi.gif",
+    "https://erikrex007yimmy.github.io/RImg/meme/imgs/cute-sunshine.gif",
+    "https://erikrex007yimmy.github.io/RImg/meme/imgs/dance-family.gif",
+    "https://erikrex007yimmy.github.io/RImg/meme/imgs/gross-bleh.gif",
+    "https://erikrex007yimmy.github.io/RImg/meme/imgs/gross-disgust.gif",
+    "https://erikrex007yimmy.github.io/RImg/meme/imgs/pog-frog-frog.gif",
+    "https://erikrex007yimmy.github.io/RImg/meme/imgs/schitts-creek-ew.gif",
+    "https://erikrex007yimmy.github.io/RImg/meme/imgs/swing-dance-swing-your-hips.gif",
+    "https://erikrex007yimmy.github.io/RImg/meme/imgs/the-simpsons-pathetic.gif"
 ]
 
 random_number = Math.floor(Math.random() * h.length);
