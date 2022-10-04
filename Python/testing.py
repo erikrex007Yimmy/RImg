f2139973c4de1024b01f5c38610fcb9bd41a25a5
erikrex007Yimmy <113575948+erikrex007Yimmy@users.noreply.github.com>
2022-10-04 17:47:05 +0200

    sad

--- Python/testing.py
+++ Python/testing.py
@@ -2,6 +2,6 @@ from urllib.request import urlopen
 
 with urlopen( 'https://erikrex007yimmy.github.io/RImg/meme/meme_api.html' ) as webpage:
     content = webpage.read().decode()
+    print(content)
     if "https://erikrex007yimmy.github.io/RImg/meme" in content:
         print("true")
-        
